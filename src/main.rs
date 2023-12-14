d6c14a887ad7f30422804286a58e35e8e45f179c
partychad <partychad@protonmail.com>
2023-12-14 20:59:31 +0100

    :recycle: Refactored main.rs to simplify reserve fetching and improve output formatting.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use ethers::providers::{Middleware, Provider};
 use ethers::types::Address;
 use std::sync::Arc;
 const RPC_URL: &str = "https://api.avax.network/ext/bc/C/rpc";
+use colorize::{self, AnsiColor};
 
 abigen!(IJOEPair, "./abis/joe_lp_abi.json");
 abigen!(IERC20, "./abis/erc20_abi.json");
@@ -15,38 +16,22 @@ async fn main() -> eyre::Result<()> {
     let block_number = provider.get_block_number().await?;
 
     println!("Chain ID: {}", chain_id);
-    println!("Block Number: {}", block_number);
+    println!("Block Number: {}\n", block_number);
     let wavax_usdt_pair_address: Address = "0x87EB2F90d7D0034571f343fb7429AE22C1Bd9F72".parse()?;
     let wavax_usdc_pair_address: Address = "0xD446eb1660F766d533BeCeEf890Df7A69d26f7d1".parse()?;
-
-    let wavax_usdt_pair = IJOEPair::new(wavax_usdt_pair_address, provider.clone());
-    let wavax_usdc_pair = IJOEPair::new(wavax_usdc_pair_address, provider.clone());
-
-    // Use the get_reserves() function to fetch the pool reserves
-    let wavax_token_address: Address = wavax_usdt_pair.get_token_x().call().await?;
-    let usdt_token_address: Address = wavax_usdt_pair.get_token_y().call().await?;
-    let wavax_token = IERC20::new(wavax_token_address, provider.clone());
-    let usdt_token = IERC20::new(usdt_token_address, provider.clone());
-    let wavax_decimal: u8 = wavax_token.decimals().call().await?;
-    let usdt_decimal: u8 = usdt_token.decimals().call().await?;
-
-    let (reserve_0, reserve_1) = wavax_usdt_pair.get_reserves().call().await?;
+    let (reserve_0, reserve_1) = get_reserves(wavax_usdt_pair_address, provider.clone()).await?;
     println!(
-        "WAVAX Supply: {}",
-        reserve_0 / 10u128.pow(wavax_decimal.into())
+        "{}\nWAVAX Supply: {}\nUSDT Supply: {}",
+        "WAVAX-USDT JOE LP".green(),
+        reserve_0,
+        reserve_1
     );
+    let (reserve_0, reserve_1) = get_reserves(wavax_usdc_pair_address, provider.clone()).await?;
     println!(
-        "USDT Supply: {}",
-        reserve_1 / 10u128.pow(usdt_decimal.into())
-    );
-    let (reserve_0, reserve_1) = wavax_usdc_pair.get_reserves().call().await?;
-    println!(
-        "WAVAX Supply: {}",
-        reserve_0 / 10u128.pow(wavax_decimal.into())
-    );
-    println!(
-        "USDC Supply: {}",
-        reserve_1 / 10u128.pow(usdt_decimal.into())
+        "{}\nWAVAX Supply: {}\nUSDC Supply: {}",
+        "WAVAX-USDT JOE LP".green(),
+        reserve_0,
+        reserve_1
     );
 
     Ok(())
