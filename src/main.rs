30dafffe26d59691670195b723d30a6269300e10
partychad <partychad@protonmail.com>
2023-12-14 19:53:53 +0100

    :sparkles: Added ERC20 abi and enhanced token supply display in main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::sync::Arc;
 const RPC_URL: &str = "https://api.avax.network/ext/bc/C/rpc";
 
 abigen!(IJOEPair, "./abis/joe_lp_abi.json");
+abigen!(IERC20, "./abis/erc20_abi.json");
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -16,12 +17,21 @@ async fn main() -> eyre::Result<()> {
     println!("Chain ID: {}", chain_id);
     println!("Block Number: {}", block_number);
     let wavax_usdt_pair_address: Address = "0x87EB2F90d7D0034571f343fb7429AE22C1Bd9F72".parse()?;
-    let wavax_usdt_pair = IJOEPair::new(wavax_usdt_pair_address, provider);
+   
+    let wavax_usdt_pair = IJOEPair::new(wavax_usdt_pair_address, provider.clone());
 
     // Use the get_reserves() function to fetch the pool reserves
+    let wavax_token_address:Address = wavax_usdt_pair.get_token_x().call().await?;
+    let usdt_token_address:Address = wavax_usdt_pair.get_token_y().call().await?;
+    let wavax_token = IERC20::new(wavax_token_address, provider.clone());
+    let usdt_token = IERC20::new(usdt_token_address, provider.clone());
+    let wavax_decimal:u8 = wavax_token.decimals().call().await?;
+    let usdt_decimal:u8 = usdt_token.decimals().call().await?;
+
+
     let (reserve_0, reserve_1) = wavax_usdt_pair.get_reserves().call().await?;
-    println!("Reserve 0: {}", reserve_0);
-    println!("Reserve 1: {}", reserve_1);
+    println!("WAVAX Supply: {}", reserve_0/10u128.pow(wavax_decimal.into()));
+    println!("USDT Supply: {}", reserve_1/ 10u128.pow(usdt_decimal.into()));
 
     Ok(())
 }
