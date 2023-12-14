f1af0eabc5718aff02df143adc5ac03e064c415a
partychad <partychad@protonmail.com>
2023-12-14 20:23:01 +0100

    :art: Improved code readability in src/main.rs by adding spaces and line breaks for clarity.

--- src/main.rs
+++ src/main.rs
@@ -18,25 +18,36 @@ async fn main() -> eyre::Result<()> {
     println!("Block Number: {}", block_number);
     let wavax_usdt_pair_address: Address = "0x87EB2F90d7D0034571f343fb7429AE22C1Bd9F72".parse()?;
     let wavax_usdc_pair_address: Address = "0xD446eb1660F766d533BeCeEf890Df7A69d26f7d1".parse()?;
-   
+
     let wavax_usdt_pair = IJOEPair::new(wavax_usdt_pair_address, provider.clone());
     let wavax_usdc_pair = IJOEPair::new(wavax_usdc_pair_address, provider.clone());
 
     // Use the get_reserves() function to fetch the pool reserves
-    let wavax_token_address:Address = wavax_usdt_pair.get_token_x().call().await?;
-    let usdt_token_address:Address = wavax_usdt_pair.get_token_y().call().await?;
+    let wavax_token_address: Address = wavax_usdt_pair.get_token_x().call().await?;
+    let usdt_token_address: Address = wavax_usdt_pair.get_token_y().call().await?;
     let wavax_token = IERC20::new(wavax_token_address, provider.clone());
     let usdt_token = IERC20::new(usdt_token_address, provider.clone());
-    let wavax_decimal:u8 = wavax_token.decimals().call().await?;
-    let usdt_decimal:u8 = usdt_token.decimals().call().await?;
-
+    let wavax_decimal: u8 = wavax_token.decimals().call().await?;
+    let usdt_decimal: u8 = usdt_token.decimals().call().await?;
 
     let (reserve_0, reserve_1) = wavax_usdt_pair.get_reserves().call().await?;
-    println!("WAVAX Supply: {}", reserve_0/10u128.pow(wavax_decimal.into()));
-    println!("USDT Supply: {}", reserve_1/ 10u128.pow(usdt_decimal.into()));
+    println!(
+        "WAVAX Supply: {}",
+        reserve_0 / 10u128.pow(wavax_decimal.into())
+    );
+    println!(
+        "USDT Supply: {}",
+        reserve_1 / 10u128.pow(usdt_decimal.into())
+    );
     let (reserve_0, reserve_1) = wavax_usdc_pair.get_reserves().call().await?;
-    println!("WAVAX Supply: {}", reserve_0/10u128.pow(wavax_decimal.into()));
-    println!("USDC Supply: {}", reserve_1/ 10u128.pow(usdt_decimal.into()));
+    println!(
+        "WAVAX Supply: {}",
+        reserve_0 / 10u128.pow(wavax_decimal.into())
+    );
+    println!(
+        "USDC Supply: {}",
+        reserve_1 / 10u128.pow(usdt_decimal.into())
+    );
 
     Ok(())
 }
