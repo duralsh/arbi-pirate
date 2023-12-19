8c725afed99391dc0febcb5ff0d936a3a6be42b4
partychad <partychad@protonmail.com>
2023-12-19 20:03:05 +0100

    :recycle: Refactored the block watcher to use a provided HTTP provider and enhanced the output to include gas usage details. Added files: src/block_watcher.rs, src/main.rs.

--- src/block_watcher.rs
+++ src/block_watcher.rs
@@ -1,19 +1,30 @@
 
 use ethers::providers::{Provider, Middleware, Ws};
-use ethers::providers::StreamExt;
-const RPC_URL: &str = "wss://api.avax.network/ext/bc/C/ws";
+use ethers::core::types::transaction::response;
+
+use std::sync::Arc;
 
 
-pub async fn watch() -> eyre::Result<()> {
-    let provider = Provider::<Ws>::connect(RPC_URL).await?;
-    let mut stream = provider.subscribe_blocks().await?;
-    while let Some(block) = stream.next().await {
-        println!(
-            "Ts: {:?}, block number: {} -> {:?}",
-            block.timestamp,
-            block.number.unwrap(),
-            block.hash.unwrap()
-        );
+pub async fn watch(http_provider:Arc<Provider<ethers::providers::Http>>) -> eyre::Result<()> {
+    loop {
+        let current_block_number = http_provider.get_block_number().await?;
+        let mut last_block_number = None;
+        match last_block_number {
+            Some(last_number) if last_number == current_block_number => {
+            },
+            _ => {
+                if let Some(full_block) = http_provider.get_block_with_txs(current_block_number).await? {
+                    for transaction in full_block.transactions {
+                        println!("Gas used: {:?}", transaction.to.unwrap());
+                    }
+                }
+                last_block_number = Some(current_block_number);
+            }
+        }
+
+        // Optional: add a delay to avoid querying too frequently
+        tokio::time::sleep(std::time::Duration::from_millis(500)).await;
     }
+
     Ok(())
 }
