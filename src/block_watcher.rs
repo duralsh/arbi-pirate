e64827f1aec5167701d9fe43f1049c356359eca7
partychad <partychad@protonmail.com>
2023-12-19 21:18:04 +0100

    :art: Improved code readability in block_watcher.rs by refining transaction address logging and removing unnecessary comments.

--- src/block_watcher.rs
+++ src/block_watcher.rs
@@ -1,5 +1,6 @@
 
 use ethers::providers::{Provider, Middleware};
+#[allow(unused_imports)]
 use ethers::core::types::transaction::response;
 
 use std::sync::Arc;
@@ -11,22 +12,21 @@ pub async fn watch(http_provider:Arc<Provider<ethers::providers::Http>>) -> eyre
 
     loop {
         let current_block_number = http_provider.get_block_number().await?;
-        println!("Current Block Number: {}", current_block_number);
         match last_block_number {
-            Some(last_number) if last_number == current_block_number => {
+            Some(last_number) if last_number == current_block_number || current_block_number < last_number => {
             },
             _ => {
                 if let Some(full_block) = http_provider.get_block_with_txs(current_block_number).await? {
                     for transaction in full_block.transactions {
-                        // println!("Block No :{} To Address: {:?}", current_block_number, transaction.to.unwrap());
+                        match transaction.to {
+                            Some(to_address) => println!("Block No: {} To Address: {:?}", current_block_number, to_address),
+                            None => println!("Block No: {} To Address: None", current_block_number),
+                        }
                     }
                 }
                 last_block_number = Some(current_block_number);
             }
         }
-
-        // Optional: add a delay to avoid querying too frequently
-        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
     }
 
 }
